b9a0325ead5d9f9148be164f81a887850f5bd5f7
Deepesh Kumar <deepeshkumar131089@gmail.com>
2023-06-14 07:31:32 +0530

    Testing for errors

--- src/test/Hooks.test.ts
+++ src/test/Hooks.test.ts
@@ -16,9 +16,36 @@ describe('Hooks test suite', () => {
 
 		it('Should return correct upperCase', () => {
 			const actual = sut.toUpperCase('abc');
-
 			expect(actual).toBe('ABC');
 			console.log('Actual test');
+		});
+
+		/* ERROR COVERAGE */
+		/* ERROR COVERAGE */
+		/* ERROR COVERAGE */
+		it('Should throw error on invalid argument - function', () => {
+			function expectError() {
+				const actual = sut.toUpperCase('');
+			}
+			expect(expectError).toThrow();
+			expect(expectError).toThrowError('Invalid argument!');
+		});
+
+		it('Should throw error on invalid argument - arrow function', () => {
+			expect(() => {
+				sut.toUpperCase('');
+			}).toThrowError('Invalid argument!');
+		});
+
+		it('Should throw error on invalid argument - try catch block', (done) => {
+			try {
+				sut.toUpperCase('');
+				done('GetStringInfo should throw error for invalid arg!');
+			} catch (error) {
+				expect(error).toBeInstanceOf(Error);
+				expect(error).toHaveProperty('message', 'Invalid argument!');
+				done();
+			}
 		});
 	});
 });
