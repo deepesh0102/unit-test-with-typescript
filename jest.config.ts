765c0334280ff6603a343ea4c196a1861c29d2af
Deepesh Kumar <deepeshkumar131089@gmail.com>
2023-06-14 07:02:19 +0530

    VSCode debug configuration

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,16 @@
 import type { Config } from '@jest/types';
 
+const baseDir = '<rootDir>/src/app';
+const baseTestDir = '<rootDir>/src/test';
+
 const config: Config.InitialOptions = {
 	verbose: true,
 	preset: 'ts-jest',
 	testEnvironment: 'node',
+	collectCoverage: true,
+	collectCoverageFrom: [`${baseDir}/**/*.ts`],
+	testMatch: [`${baseTestDir}/**/*.ts`],
+	roots: ['<rootDir>/src/', '<rootDir>/src/test/'],
 };
 
 export default config;
